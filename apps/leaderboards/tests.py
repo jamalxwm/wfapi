ea6d02bbb5cdc83a37ecb0068727f41e3eb1130c
jamalxwm <jamal@letswayfare.com>
2023-12-01 16:55:31 -0500

    Adds individual leaderboard behaviour

--- apps/leaderboards/tests.py
+++ apps/leaderboards/tests.py
@@ -3,7 +3,7 @@ from faker import Faker
 from django_redis import get_redis_connection
 from .views import Leaderboards as LeaderboardViews
 
-class TestLeaderboard:
+class TestMainLeaderboard:
     @pytest.fixture(scope='class', autouse=True)
     def redis(self):
         conn = get_redis_connection('default')
@@ -17,36 +17,127 @@ class TestLeaderboard:
         users.insert(1, 'static_user')
         return users
 
-    def test_add_user(self, redis, users):
+    @pytest.fixture
+    def leaderboard(self):
+        return LeaderboardViews('main_leaderboard')
+
+    def test_add_user(self, redis, users, leaderboard):
         # Add users to the leaderboard sorted set
         for index, name in enumerate(users):
-            LeaderboardViews.add_user(name, index)
+            leaderboard.add_user(name, index)
 
-        assert redis.zcard('leaderboard') == 10
+        assert redis.zcard('main_leaderboard') == 10
 
-    def test_user_position(self, redis):
-        position = LeaderboardViews.get_user_rank('static_user')
+    def test_user_position(self, redis, leaderboard):
+        position = leaderboard.get_user_rank('static_user')
 
-        assert position - 1 == redis.zrevrank('leaderboard', 'static_user')
+        assert position - 1 == redis.zrevrank('main_leaderboard', 'static_user')
 
-    def test_update_user_position(self, redis):
+    def test_update_user_position(self, redis, leaderboard):
         # Update user score and check if it's updated correctly
-        start_position = LeaderboardViews.get_user_rank('static_user') - 1
+        start_position = leaderboard.get_user_rank('static_user') - 1
         spaces_to_move = 5
-        LeaderboardViews.update_user_rank_by_spaces('static_user', spaces_to_move)
+        leaderboard.update_user_rank_by_spaces('static_user', spaces_to_move)
 
-        assert redis.zrevrank('leaderboard', 'static_user') == start_position - spaces_to_move
+        assert redis.zrevrank('main_leaderboard', 'static_user') == start_position - spaces_to_move
 
-    def test_update_user_position_if_negative_space_jumps(self, redis):
+    def test_update_user_position_if_negative_space_jumps(self, redis, leaderboard):
         # Update user score and check if it's updated correctly
-        spaces_to_move = 5
-        LeaderboardViews.update_user_rank_by_spaces('static_user', spaces_to_move)
+        spaces_to_move = 20
+        leaderboard.update_user_rank_by_spaces('static_user', spaces_to_move)
 
-        assert redis.zrevrank('leaderboard', 'static_user') == 0
+        assert redis.zrevrank('main_leaderboard', 'static_user') == 0
+
+    def test_get_leaderboard_length(self, leaderboard):
+        assert leaderboard.get_leaderboard_length() == 10
+
+    def test_delete_user(self, redis, leaderboard):
+        leaderboard.delete_user('static_user')
+        assert redis.zcard('main_leaderboard') == 9
+
+
+class TestIndividualLeaderboard:
+    @pytest.fixture(scope='class', autouse=True)
+    def redis(self):
+        conn = get_redis_connection('default')
+        yield conn
+        conn.flushall()
+
+    @pytest.fixture
+    def users(self):
+        fake = Faker()
+        users = [fake.name() for _ in range(9)]
+        users.insert(1, 'static_user')
+        return users
+
+    @pytest.fixture
+    def individual_leaderboard(self):
+        return LeaderboardViews('individual_rankings')
+
+    @pytest.fixture
+    def main_leaderboard(self):
+        return LeaderboardViews('main_leaderboard')
+
+    def test_add_user(self, redis, users, individual_leaderboard, main_leaderboard):
+        # Add users to individual rankings with their main leaderboard ranking as the score
+        for index, name in enumerate(users):
+            main_leaderboard.add_user(name, index)
+
+        # Adjust individual ranking scores after main leaderboard is fully populated 
+        for user in users:
+            individual_leaderboard._populate_individual_rankings(user)
+
+        # Get the rank of the 'static_user' in the 'main_leaderboard'
+        main_leaderboard_rank = redis.zrevrank('main_leaderboard', 'static_user')
+
+        # The score of the 'static_user' in the 'individual_rankings' should be equal to their rank in the 'main_leaderboard'
+        individual_ranking_score = redis.zscore('individual_rankings', 'static_user')
+
+        # The score of the 'static_user' in the 'individual_rankings' should be equal to their rank in the 'main_leaderboard'
+        assert individual_ranking_score == main_leaderboard_rank
+
+    def test_should_update_individual_ranking_when_main_leaderboard_increases(self, redis, individual_leaderboard, main_leaderboard):
+        # Get the initial score of a user ('static_user') in the individual leaderboard
+        initial_ranking_score = redis.zscore('individual_rankings', 'static_user')
+
+        # Define the number of spaces the user should move up in the leaderboard
+        spaces_to_move = 5
+
+        # Update the rank of the user in the main leaderboard by moving it up by 'spaces_to_move'
+        main_leaderboard.update_user_rank_by_spaces('static_user', spaces_to_move)
+
+        # Get the rank of the user in the main leaderboard after moving it
+        main_leaderboard_rank = redis.zrevrank('main_leaderboard', 'static_user')
+
+        # Get the final score of the user in the individual leaderboard after the update
+        final_ranking_score = redis.zscore('individual_rankings', 'static_user')
+
+        # Assert that the final score in the individual leaderboard equals the initial score minus the moved spaces
+        # Also, assert that this final score equals the rank of the user in the main leaderboard
+        assert final_ranking_score == initial_ranking_score - spaces_to_move == main_leaderboard_rank
+
+class TestTeamLogic:
+    @pytest.fixture(scope='class', autouse=True)
+    def redis(self):
+        conn = get_redis_connection('default')
+        yield conn
+        conn.flushall()
+
+    @pytest.fixture
+    def users(self):
+        fake = Faker()
+        users = [fake.name() for _ in range(98)]
+        users.insert(0, 'team_member_A')
+        users.insert(50, 'team_member_B')
+        return users
+
+    @pytest.fixture
+    def individual_leaderboard(self):
+        return LeaderboardViews('individual_rankings')
+
+    @pytest.fixture
+    def main_leaderboard(self):
+        return LeaderboardViews('main_leaderboard')
     
-    def test_get_leaderboard_length(self):
-        assert LeaderboardViews.get_leaderboard_length() == 10
-
-    def test_delete_user(self, redis):
-        LeaderboardViews.delete_user('static_user')
-        assert redis.zcard('leaderboard') == 9
+    def test_should_create_with_highest_members_score(self, redis, main_leaderboard):
+        
--- apps/leaderboards/views.py
+++ apps/leaderboards/views.py
@@ -2,50 +2,67 @@ from django_redis import get_redis_connection
 
 
 class Leaderboards:
-    conn = get_redis_connection("default")
 
-    @staticmethod
-    def add_user(user, score):
-        Leaderboards.conn.zadd('leaderboard', {user: score})
+    def __init__(self, leaderboard, conn=None):
+        self.leaderboard = leaderboard
+        self.conn = conn if conn else get_redis_connection("default")
 
-    @staticmethod
-    def delete_user(user):
-        Leaderboards.conn.zrem('leaderboard', user)
+    def add_user(self, user, score):
+        # Add the user to the main leaderboard
+        self.conn.zadd('main_leaderboard', {user: score})
 
-    @staticmethod
-    def update_user_rank_by_spaces(user, spaces):
-        current_score = Leaderboards.conn.zscore('leaderboard', user)
-        current_rank = Leaderboards.get_user_rank(user) - 1
-        
+    def delete_user(self, user):
+        self.conn.zrem(self.leaderboard, user)
+
+    def update_user_rank_by_spaces(self, user, spaces):
+        current_score = self.conn.zscore(self.leaderboard, user)
+        current_rank = self.get_user_rank(user) - 1
+
         if current_rank is None:
-            Leaderboards.add_user(user, 0)
-            Leaderboards.update_user_rank_by_spaces(user, spaces)
-        
+            self.add_user(user, 0)
+            self.update_user_rank_by_spaces(user, spaces)
+
         target_rank = max(0, current_rank - spaces)
-        return Leaderboards._move_user_to_rank(user, current_score, target_rank)
-    
-    @staticmethod
-    def get_user_rank(user):
-        return Leaderboards.conn.zrevrank('leaderboard', user) + 1
-    
-    @staticmethod
-    def get_leaderboard_length():
-        return Leaderboards.conn.zcard('leaderboard')
-    
-    @staticmethod
-    def _get_user_by_rank(rank):
-        return Leaderboards.conn.zrevrange('leaderboard', rank, rank, withscores=True)
-    
-    @staticmethod
-    def _move_user_to_rank(user, current_score, target_rank):
-        user_at_target_rank = Leaderboards._get_user_by_rank(target_rank)
-        target_score = current_score + 0.01 # Default score if no user at target rank
+        self._update_individual_rankings(user, target_rank)
+        return self._move_user_to_rank(user, current_score, target_rank)
+
+    def get_user_rank(self, user):
+        rank = self.conn.zrevrank(self.leaderboard, user)
+        return rank + 1 if rank is not None else None
+
+    def get_leaderboard_length(self):
+        return self.conn.zcard(self.leaderboard)
+
+    def _get_user_by_rank(self, rank):
+        return self.conn.zrevrange(self.leaderboard, rank, rank, withscores=True)
+
+    def _move_user_to_rank(self, user, current_score, target_rank):
+        user_at_target_rank = self._get_user_by_rank(target_rank)
+
+        # Look for next user on the leaderboard if none at target rank
+        while not user_at_target_rank and target_rank < self.get_leaderboard_length():
+            target_rank += 1
+            user_at_target_rank = self._get_user_by_rank(target_rank)
 
         if user_at_target_rank:
             _, target_score = user_at_target_rank[0]
+        else:
+            target_score = current_score + 1  # Default score if no user at target rank
 
         new_score = target_score - current_score + 0.01
-        Leaderboards.conn.zincrby('leaderboard', new_score, user)
+        self.conn.zincrby(self.leaderboard, new_score, user)
 
         return f"Moved {user} to rank {target_rank + 1}"
-   
+
+    def _populate_individual_rankings(self, user):
+            user_rank_main = self.conn.zrevrank('main_leaderboard', user)
+            
+            if user_rank_main is not None:
+                self.conn.zadd('individual_rankings', {user: user_rank_main})
+            else:
+                print(f"Rank for user {user} not found in main leaderboard.")
+
+    def _update_individual_rankings(self, user, new_rank):
+        return self.conn.zadd('individual_rankings', {user: new_rank})
+
+    def create_new
