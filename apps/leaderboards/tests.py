583001094f176c5e5c8adce74af1ca379c964bc6
jamalxwm <jamal@letswayfare.com>
2023-12-02 16:45:43 -0500

    Updates user_rank_by_spaces method to handle teams

--- apps/leaderboards/tests.py
+++ apps/leaderboards/tests.py
@@ -31,11 +31,11 @@ class TestMainLeaderboard:
     def test_user_position(self, redis, leaderboard):
         position = leaderboard.get_user_rank('static_user')
 
-        assert position - 1 == redis.zrevrank('main_leaderboard', 'static_user')
+        assert position == redis.zrevrank('main_leaderboard', 'static_user')
 
     def test_update_user_position(self, redis, leaderboard):
         # Update user score and check if it's updated correctly
-        start_position = leaderboard.get_user_rank('static_user') - 1
+        start_position = leaderboard.get_user_rank('static_user')
         spaces_to_move = 5
         leaderboard.update_user_rank_by_spaces('static_user', spaces_to_move)
 
@@ -91,15 +91,15 @@ class TestIndividualLeaderboard:
         main_leaderboard_rank = redis.zrevrank('main_leaderboard', 'static_user')
 
         # The score of the 'static_user' in the 'individual_rankings' should be equal to their rank in the 'main_leaderboard'
-        individual_ranking_score = redis.zscore('individual_rankings', 'static_user')
+        individual_ranking_score = redis.hget('individual_rankings', 'static_user')
 
         # The score of the 'static_user' in the 'individual_rankings' should be equal to their rank in the 'main_leaderboard'
-        assert individual_ranking_score == main_leaderboard_rank
+        assert int(individual_ranking_score) == main_leaderboard_rank
 
     def test_should_update_individual_ranking_when_main_leaderboard_increases(self, redis, individual_leaderboard, main_leaderboard):
         # Get the initial score of a user ('static_user') in the individual leaderboard
-        initial_ranking_score = redis.zscore('individual_rankings', 'static_user')
-
+        initial_ranking_score = redis.hget('individual_rankings', 'static_user')
+        
         # Define the number of spaces the user should move up in the leaderboard
         spaces_to_move = 5
 
@@ -110,11 +110,11 @@ class TestIndividualLeaderboard:
         main_leaderboard_rank = redis.zrevrank('main_leaderboard', 'static_user')
 
         # Get the final score of the user in the individual leaderboard after the update
-        final_ranking_score = redis.zscore('individual_rankings', 'static_user')
+        final_ranking_score = redis.hget('individual_rankings', 'static_user')
 
         # Assert that the final score in the individual leaderboard equals the initial score minus the moved spaces
         # Also, assert that this final score equals the rank of the user in the main leaderboard
-        assert final_ranking_score == initial_ranking_score - spaces_to_move == main_leaderboard_rank
+        assert int(final_ranking_score) == int(initial_ranking_score) - spaces_to_move == main_leaderboard_rank
 
 class TestTeamLogic:
     @pytest.fixture(scope='class', autouse=True)
@@ -140,19 +140,65 @@ class TestTeamLogic:
         return LeaderboardViews('main_leaderboard')
 
     @pytest.fixture(scope='class', autouse=True)
-    def seed_db(self, users, main_leaderboard):
+    def seed_db(self, users, main_leaderboard, redis):
         for index, name in enumerate(users):
             main_leaderboard.add_user(name, index)
-    
+        
+        leaderboard = redis.zrange('main_leaderboard', 0, -1, withscores=True)
+
+        for user, _ in leaderboard:
+            main_leaderboard._populate_individual_rankings(user)
+         
     @pytest.mark.run(order=1)
     def test_should_create_with_highest_members_score(self, redis, main_leaderboard):
         score1 = redis.zscore('main_leaderboard', 'team_member_A')
         score2 = redis.zscore('main_leaderboard', 'team_member_B')
         main_leaderboard.create_new_team('team_member_A', 'team_member_B')
-
+        
         assert redis.zscore('main_leaderboard', 'team_member_A_team_member_B') == max(score1, score2)
     
     @pytest.mark.run(order=2)
-    def test_should_replace_team_members_with_team(self, redis, main_leaderboard):
-        assert redis.zrank('main_leaderboard', 'team_member_A') == None
-        assert redis.zrank('main_leaderboard', 'team_member_B') == None
+    def test_should_replace_team_members_with_team(self, redis):
+        assert redis.zrevrank('main_leaderboard', 'team_member_A') == None
+        assert redis.zrevrank('main_leaderboard', 'team_member_B') == None
+
+    @pytest.mark.run(order=3)
+    def test_should_add_team_members_to_team_hashset(self, redis):
+        assert redis.hget('user_teams', 'team_member_A') != None
+        assert redis.hget('user_teams', 'team_member_B') != None
+
+    @pytest.fixture
+    def leaderboard_setup(self, redis, main_leaderboard):
+        team_start_rank = main_leaderboard.get_user_rank('team_member_A_team_member_B')
+        spaces_to_move = 5
+        initial_user_A_rank = redis.hget('individual_rankings', 'team_member_A')
+        main_leaderboard.update_user_rank_by_spaces('team_member_A', spaces_to_move)
+        team_final_rank = main_leaderboard.get_user_rank('team_member_A_team_member_B')
+        return team_start_rank, spaces_to_move, team_final_rank, initial_user_A_rank
+
+    @pytest.mark.run(order=4)
+    def test_should_update_team_rank_on_leaderboard(self, leaderboard_setup, redis):
+        team_start_rank, spaces_to_move, team_final_rank, _ = leaderboard_setup
+        assert redis.zrevrank('main_leaderboard', 'team_member_A_team_member_B') == team_start_rank - spaces_to_move == team_final_rank
+
+    def test_should_only_update_ranking_of_initiator(self, leaderboard_setup, redis, main_leaderboard):
+        _, spaces_to_move, _, initial_user_A_rank = leaderboard_setup
+
+        # Assert User A's rank on individual_rankings has updated by 5
+        final_user_A_rank = int(redis.hget('individual_rankings', 'team_member_A'))
+        assert final_user_A_rank == int(initial_user_A_rank) - spaces_to_move
+
+        # Assert User B's rank on individual_rankings is unchanged
+        initial_user_B_rank = redis.hget('individual_rankings', 'team_member_B')
+
+        assert redis.hget('individual_rankings', 'team_member_B') == initial_user_B_rank
+
+        # Assert user A and user B are not on the main leaderboard
+        assert main_leaderboard.get_user_rank('team_member_A') is None
+        assert main_leaderboard.get_user_rank('team_member_B') is None
+
+    @pytest.mark.run(order=5)
+    def test_should_reinstate_team_members_position(self, individual_leaderboard, main_leaderboard):
+        main_leaderboard.delete_team('team_member_B')
+        assert main_leaderboard.get_user_rank('team_member_A') == 94
+        assert main_leaderboard.get_user_rank('team_member_B') == 49
--- apps/leaderboards/views.py
+++ apps/leaderboards/views.py
@@ -14,21 +14,28 @@ class Leaderboards:
     def delete_user(self, user):
         self.conn.zrem(self.leaderboard, user)
 
-    def update_user_rank_by_spaces(self, user, spaces):
-        current_score = self.conn.zscore(self.leaderboard, user)
-        current_rank = self.get_user_rank(user) - 1
+    def update_user_rank_by_spaces(self, initiator, spaces):
+        user = initiator
+        is_team = self.conn.hexists('user_teams', initiator)
 
-        if current_rank is None:
+        if is_team:
+            user = self._get_team_id(initiator)
+  
+        current_score = self.conn.zscore(self.leaderboard, user)
+        current_rank = self.get_user_rank(user)
+
+        if current_rank is None and not is_team:
             self.add_user(user, 0)
             self.update_user_rank_by_spaces(user, spaces)
+            return
 
         target_rank = max(0, current_rank - spaces)
-        self._update_individual_rankings(user, target_rank)
+        self._increment_individual_rankings_by_spaces(initiator, spaces)
         return self._move_user_to_rank(user, current_score, target_rank)
 
     def get_user_rank(self, user):
         rank = self.conn.zrevrank(self.leaderboard, user)
-        return rank + 1 if rank is not None else None
+        return rank if rank is not None else None
 
     def get_leaderboard_length(self):
         return self.conn.zcard(self.leaderboard)
@@ -36,7 +43,7 @@ class Leaderboards:
     def _get_user_by_rank(self, rank):
         return self.conn.zrevrange(self.leaderboard, rank, rank, withscores=True)
 
-    def _move_user_to_rank(self, user, current_score, target_rank):
+    def _move_user_to_rank(self, user, current_score, target_rank, skip_value=0.01):
         user_at_target_rank = self._get_user_by_rank(target_rank)
 
         # Look for next user on the leaderboard if none at target rank
@@ -49,7 +56,7 @@ class Leaderboards:
         else:
             target_score = current_score + 1  # Default score if no user at target rank
 
-        new_score = target_score - current_score + 0.01
+        new_score = target_score - current_score + skip_value
         self.conn.zincrby(self.leaderboard, new_score, user)
 
         return f"Moved {user} to rank {target_rank + 1}"
@@ -58,12 +65,12 @@ class Leaderboards:
             user_rank_main = self.conn.zrevrank('main_leaderboard', user)
             
             if user_rank_main is not None:
-                self.conn.zadd('individual_rankings', {user: user_rank_main})
+                self.conn.hset('individual_rankings', user, user_rank_main)
             else:
                 print(f"Rank for user {user} not found in main leaderboard.")
 
-    def _update_individual_rankings(self, user, new_rank):
-        return self.conn.zadd('individual_rankings', {user: new_rank})
+    def _increment_individual_rankings_by_spaces(self, user, spaces):
+        return self.conn.hincrby('individual_rankings', user, -spaces)
 
     def create_new_team(self, user1, user2):
         team_id = f'{user1}_{user2}'
@@ -72,8 +79,21 @@ class Leaderboards:
         score2 = self.conn.zscore('main_leaderboard', user2)
         
         team_score = max(score1, score2) if score1 and score2 else score1 or score2
+
+        self.conn.hset('user_teams', user1, team_id)
+        self.conn.hset('user_teams', user2, team_id)
         
         self.conn.zrem('main_leaderboard', user1)
         self.conn.zrem('main_leaderboard', user2)
 
         return self.add_user(team_id, team_score)
+
+    def _get_team_id(self, user):
+       return self.conn.hget('user_teams', user)
+
+    def delete_team(self, initiator):
+        team_id = self._get_team_id(initiator)
+        # Get the team ID to find both users
+        # Find users final ranking in individual rankings
+        # Reinstate users to main leaderboard to correct rank (use skip value 0)
+        # Remove team from leaderboard and user_teams
