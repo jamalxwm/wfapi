fd4a83413a1c1c63a35b82daab6a5d8b1e0399d1
jamalxwm <jamal@letswayfare.com>
2023-12-29 13:39:45 -0500

    Adds tests and refactors Task model save to handle adding new tasks with existing display order

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -1,58 +1,93 @@
-from django.db import models
+from django.db import models, transaction
+from django.db.models import F
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
 
+
 class Tasks(models.Model):
     TARGET_DEMO_CHOICES = [
-        ('EXPAT', 'Expat'),
-        ('PARENT', 'Parent'),
-        ('SPORTS', 'Sports'),
-        ('FESTIVAL', 'Festival'),
+        ("EXPAT", "Expat"),
+        ("PARENT", "Parent"),
+        ("SPORTS", "Sports"),
+        ("FESTIVAL", "Festival"),
     ]
     title = models.CharField(max_length=255)
     description = models.TextField(max_length=255, blank=True)
     uri = models.URLField(blank=True, null=True)
     queue_jumps = models.PositiveBigIntegerField(blank=False, default=10)
-    
-    prerequisite = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='dependent')
-    max_repetitions = models.PositiveIntegerField(default=1, help_text="Maximum number of times a user can repeat a task.")
+
+    prerequisite = models.ForeignKey(
+        "self",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="dependent",
+    )
+    max_repetitions = models.PositiveIntegerField(
+        default=1, help_text="Maximum number of times a user can repeat a task."
+    )
     display_order = models.PositiveIntegerField(unique=True)
-    total_global_completions = models.PositiveIntegerField(default=0, help_text="Total completions by all users.")
-    pending_global_completions = models.PositiveIntegerField(default=0, help_text="Number of users who have this task available but have not completed it.")
+    total_global_completions = models.PositiveIntegerField(
+        default=0, help_text="Total completions by all users."
+    )
+    pending_global_completions = models.PositiveIntegerField(
+        default=0,
+        help_text="Number of users who have this task available but have not completed it.",
+    )
     is_hidden = models.BooleanField(default=False, null=False)
-    target_demographic = models.CharField(max_length=25, choices=TARGET_DEMO_CHOICES, blank=True)
+    target_demographic = models.CharField(
+        max_length=25, choices=TARGET_DEMO_CHOICES, blank=True
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def clean(self):
         # If the task is hidden, target_demographic must be set
         if self.is_hidden and not self.target_demographic:
-            raise ValidationError({
-                'target_demographic': _("This field must be set when the task is hidden.")
-            })
+            raise ValidationError(
+                {
+                    "target_demographic": _(
+                        "This field must be set when the task is hidden."
+                    )
+                }
+            )
 
         # If the task is not hidden, target_demographic must be blank
         if not self.is_hidden and self.target_demographic:
-            raise ValidationError({
-                'target_demographic': _("This field must be blank when the task is not hidden.")
-            })
-
+            raise ValidationError(
+                {
+                    "target_demographic": _(
+                        "This field must be blank when the task is not hidden."
+                    )
+                }
+            )
+            
     def save(self, *args, **kwargs):
-        self.full_clean()  # Call the full_clean method to run validation
-        super().save(*args, **kwargs)
+        with transaction.atomic():
+            # If the task is being added, not updated
+            if self._state.adding:
+                # If a task with the same display_order exists, increment all subsequent tasks
+                if Tasks.objects.filter(display_order=self.display_order).exists():
+                    Tasks.objects.filter(display_order__gte=self.display_order)\
+                        .order_by('-display_order')\
+                        .update(display_order=F('display_order') + 1)
+            
+            self.full_clean()  # Validate now, after re-ordering logic
+            super().save(*args, **kwargs)  # Save the current instance
 
     def __str__(self):
         return self.title
 
+
 class UserTasks(models.Model):
     STATE_CHOICES = [
-        ('AVAILABLE', 'Available'),
-        ('LOCKED', 'Locked'),
-        ('HIDDEN', 'Hidden'),
-        ('COMPLETED', 'Completed')
+        ("AVAILABLE", "Available"),
+        ("LOCKED", "Locked"),
+        ("HIDDEN", "Hidden"),
+        ("COMPLETED", "Completed"),
     ]
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -62,10 +97,10 @@ class UserTasks(models.Model):
 
     def complete(self):
         # Mark the task as completed and handle related logic.
-        if self.state != 'COMPLETED':
+        if self.state != "COMPLETED":
             self.repetitions += 1
             if self.repetitions >= self.task.max_repetitions:
-                self.state = 'COMPLETED'
+                self.state = "COMPLETED"
             # Unlock dependent tasks
             self._unlock_dependent_tasks()
             self.save()
@@ -73,15 +108,17 @@ class UserTasks(models.Model):
     def _unlock_dependent_tasks(self):
         # Unlock tasks that are dependent on the completion of this task.
         for dependent in self.task.dependent.all():
-            user_task, created = UserTasks.objects.get_or_create(user=self.user, task=dependent)
-            if created or user_task.state != 'COMPLETED':
-                user_task.state = 'AVAILABLE'
+            user_task, created = UserTasks.objects.get_or_create(
+                user=self.user, task=dependent
+            )
+            if created or user_task.state != "COMPLETED":
+                user_task.state = "AVAILABLE"
                 user_task.save()
 
     def save(self, *args, **kwargs):
         # The save method now only calls the superclass's save method.
         # All business logic is handled in other methods.
         super(UserTasks, self).save(*args, **kwargs)
-    
+
     def __str__(self):
         return f"{self.user.username} - {self.task.title}"
--- apps/tasks/tests.py
+++ apps/tasks/tests.py
@@ -2,7 +2,6 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from apps.tasks.models import Tasks, UserTasks
-from apps.users.models import UserProfile
 
 User = get_user_model()
 
@@ -10,19 +9,18 @@ User = get_user_model()
 class TaskTestCase(TestCase):
     def setUp(self):
         # Create some tasks
-        self.task1 = Tasks.objects.create(title="Task 1", display_order=1, max_repetitions=1)
+        self.task1 = Tasks.objects.create(
+            title="Task 1", display_order=1, max_repetitions=1
+        )
         self.task2 = Tasks.objects.create(
-            title="Task 2",
-            prerequisite=self.task1,
-            display_order=2,
-            max_repetitions=5
+            title="Task 2", prerequisite=self.task1, display_order=2, max_repetitions=5
         )
         self.task3 = Tasks.objects.create(
             title="Task 3",
             display_order=3,
             is_hidden=True,
             target_demographic="EXPAT",
-            max_repetitions=1
+            max_repetitions=1,
         )
         self.user = User.objects.create_user(
             email="testuser@test.com",
@@ -68,33 +66,39 @@ class TaskTestCase(TestCase):
             "task__display_order"
         )
         total_tasks_count = Tasks.objects.count()
-        
+
         self.assertEqual(user_tasks.count(), total_tasks_count)
         self.assertEqual(user_tasks[2].task.title, "Task 3")
         self.assertEqual(user_tasks[2].state, "AVAILABLE")
-
+        
     def test_single_repitition_task_completes(self):
         user_task = UserTasks.objects.create(user=self.user, task=self.task1)
         user_task.complete()
 
-        self.assertEqual(user_task.state, 'COMPLETED')
+        self.assertEqual(user_task.state, "COMPLETED")
 
     def test_multi_repetition_task_stays_available(self):
-        user_task = UserTasks.objects.create(user=self.user, task=self.task2, state='AVAILABLE')
-        user_task.complete() # First repetition
-        user_task.complete() # Second repetition
+        user_task = UserTasks.objects.create(
+            user=self.user, task=self.task2, state="AVAILABLE"
+        )
+        user_task.complete()  # First repetition
+        user_task.complete()  # Second repetition
 
-        self.assertEqual(user_task.state, 'AVAILABLE') 
+        self.assertEqual(user_task.state, "AVAILABLE")
         self.assertEqual(user_task.repetitions, 2)
 
     def test_dependent_tasks_unlock(self):
-        prerequisite_user_task, _ = UserTasks.objects.get_or_create(user=self.user, task=self.task1, defaults={'state': 'AVAILABLE'})
-        dependent_user_task, _ = UserTasks.objects.get_or_create(user=self.user, task=self.task2, defaults={'state': 'LOCKED'})
-        
+        prerequisite_user_task, _ = UserTasks.objects.get_or_create(
+            user=self.user, task=self.task1, defaults={"state": "AVAILABLE"}
+        )
+        dependent_user_task, _ = UserTasks.objects.get_or_create(
+            user=self.user, task=self.task2, defaults={"state": "LOCKED"}
+        )
+
         prerequisite_user_task.complete()
         dependent_user_task.refresh_from_db()
-        
-        self.assertEqual(dependent_user_task.state, 'AVAILABLE')
+
+        self.assertEqual(dependent_user_task.state, "AVAILABLE")
 
     def test_repetitions_does_not_surpass_max(self):
         MAX_REPITITIONS = 1
@@ -106,47 +110,80 @@ class TaskTestCase(TestCase):
         self.assertEqual(user_task.repetitions, MAX_REPITITIONS)
 
     def test_completed_task_does_not_complete_again(self):
-        user_task = UserTasks.objects.create(user=self.user, task=self.task1, state='COMPLETED')
+        user_task = UserTasks.objects.create(
+            user=self.user, task=self.task1, state="COMPLETED"
+        )
         initial_task_state = user_task.state
         user_task.complete()
-        
-        self.assertEqual(user_task.state, 'COMPLETED')
 
-# class TaskModelTests(TestCase):
-#     def test_hidden_task_requires_target_demographic(self):
-#         # Create a Task with is_hidden=True but no target_demographic
-#         task = Tasks(
-#             title="Task 1", display_order=1, is_hidden=True, target_demographic=""
-#         )
+        self.assertEqual(user_task.state, initial_task_state)
 
-#         # Expect this to raise a ValidationError
-#         with self.assertRaises(ValidationError):
-#             task.full_clean()
 
-#     def test_visible_task_cannot_have_target_demographic(self):
-#         # Create a Task with is_hidden=False and a target_demographic
-#         task = Tasks(title='Task 1', display_order=1, is_hidden=False, target_demographic="EXPAT")
+class TaskModelTests(TestCase):
 
-#         # Expect this to raise a ValidationError
-#         with self.assertRaises(ValidationError):
-#             task.full_clean()
+    def test_hidden_task_requires_target_demographic(self):
+        # Create a Task with is_hidden=True but no target_demographic
+        task = Tasks(
+            title="Task 1", display_order=1, is_hidden=True, target_demographic=""
+        )
 
-#     def test_hidden_task_with_valid_target_demographic(self):
-#         # Create a Task with is_hidden=True and a valid target_demographic
-#         task = Tasks(title='Task 1', display_order=1, is_hidden=True, target_demographic="EXPAT")
+        # Expect this to raise a ValidationError
+        with self.assertRaises(ValidationError):
+            task.full_clean()
 
-#         # This should not raise a ValidationError
-#         try:
-#             task.full_clean()
-#         except ValidationError:
-#             self.fail("full_clean() raised ValidationError unexpectedly!")
+    def test_visible_task_cannot_have_target_demographic(self):
+        # Create a Task with is_hidden=False and a target_demographic
+        task = Tasks(title='Task 1', display_order=1, is_hidden=False, target_demographic="EXPAT")
 
-#     def test_visible_task_without_target_demographic(self):
-#         # Create a Task with is_hidden=False and no target_demographic
-#         task = Tasks(title='Task 1', display_order=1, is_hidden=False, target_demographic="")
+        # Expect this to raise a ValidationError
+        with self.assertRaises(ValidationError):
+            task.full_clean()
 
-#         # This should not raise a ValidationError
-#         try:
-#             task.full_clean()
-#         except ValidationError:
-#             self.fail("full_clean() raised ValidationError unexpectedly!")
+    def test_hidden_task_with_valid_target_demographic(self):
+        # Create a Task with is_hidden=True and a valid target_demographic
+        task = Tasks(title='Task 1', display_order=1, is_hidden=True, target_demographic="EXPAT")
+
+        # This should not raise a ValidationError
+        try:
+            task.full_clean()
+        except ValidationError:
+            self.fail("full_clean() raised ValidationError unexpectedly!")
+
+    def test_visible_task_without_target_demographic(self):
+        # Create a Task with is_hidden=False and no target_demographic
+        task = Tasks(title='Task 1', display_order=1, is_hidden=False, target_demographic="")
+
+        # This should not raise a ValidationError
+        try:
+            task.full_clean()
+        except ValidationError:
+            self.fail("full_clean() raised ValidationError unexpectedly!")
+
+class TaskModelDisplayOrder(TestCase):
+    def setUp(self):
+        # Create initial tasks with distinct display_orders
+        Tasks.objects.create(title='Task 1', display_order=1)
+        Tasks.objects.create(title='Task 2', display_order=2)
+
+    def test_unique_display_order(self):
+        # Test that a new task with a unique display order doesn't affect others
+        task = Tasks(title='Unique Order Task', display_order=3)
+        task.save()
+        self.assertEqual(Tasks.objects.get(title='Task 1').display_order, 1)
+        self.assertEqual(Tasks.objects.get(title='Task 2').display_order, 2)
+        self.assertEqual(Tasks.objects.get(title='Unique Order Task').display_order, 3)
+    
+    def test_duplicate_display_order(self):
+        # Test that a new task with a duplicate display order increments others
+        task = Tasks(title='Duplicate Order Task', display_order=2)
+        task.save()
+
+        # Refresh the objects from the database
+        task1 = Tasks.objects.get(title='Task 1')
+        task2 = Tasks.objects.get(title='Task 2')
+        new_task = Tasks.objects.get(title='Duplicate Order Task')
+
+        # Check the display orders to ensure they have been incremented properly
+        self.assertEqual(task1.display_order, 1)  # Should remain unchanged
+        self.assertEqual(task2.display_order, 3)  # Should be incremented
+        self.assertEqual(new_task.display_order, 2)  # Should take the place of task 2
